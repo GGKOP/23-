96212959f01c814b21f4488bfdbc3631f5ae45f6
kaka <1097598746@qq.com>
2023-08-14 11:21:21 +0800

    8/13 last

--- logger.go
+++ logger.go
@@ -1,9 +1,14 @@
 package gei
 
 import (
+	"fmt"
 	"log"
 	"time"
 )
+
+func Quiksendemail() {
+	fmt.Println(" email  success")
+}
 
 func Logger() HandlerFunc {
 	return func(c *Context) {
@@ -13,15 +18,54 @@ func Logger() HandlerFunc {
 	}
 }
 
-func Login() HandlerFunc {
+func Signup() HandlerFunc {
 	return func(c *Context) {
-		newdata := c.Newrequestdata()
+
+		type RequestData struct {
+			Email    string `validate:"email"`
+			Password string `validate:"password"`
+			Phone    string `validate:"phone"`
+		}
+
+		newdata := &RequestData{
+			Email:    c.Req.FormValue("email"),
+			Phone:    c.Req.FormValue("phone"),
+			Password: c.Req.FormValue("password"),
+		}
+
 		if validatestruct(newdata) {
-			newdata.Quiksendemail()
-			log.Printf("success")
+			Quiksendemail()
+			log.Printf(" sign  up success")
 		} else {
 			log.Printf(" email false")
 		}
+		c.Next()
 	}
 
+}
+
+func login() HandlerFunc {
+	return func(c *Context) {
+
+		type loginfo struct {
+			Userid   string `validate:"userid"`
+			Password string `validate:"password"`
+		}
+		newloginfo := &loginfo{
+			Userid:   c.Req.FormValue("userid"),
+			Password: c.Req.FormValue("password"),
+		}
+
+		if validatestruct(newloginfo) {
+			log.Printf("log in success")
+		}
+		c.Next()
+	}
+}
+
+func logout() HandlerFunc {
+	return func(c *Context) {
+		log.Printf("log out success")
+		c.Next()
+	}
 }
